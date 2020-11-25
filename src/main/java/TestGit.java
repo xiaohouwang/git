9daf392ff192c046c039f555e444d88c9f4148fb
xiaohouwang <74907556+xiaohouwang@users.noreply.github.com>
2020-11-25 09:39:51 +0800

    Update TestGit.java
    用户C修改了代码，并提交到了中央仓库...

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -8,5 +8,6 @@ public class TestGit {
         System.out.println("这是在分支中添加的代码");
         System.out.println("我是用户B，修改了代码，提交到远程仓库");
         System.out.println("用户A修改了代码，并提交到了中央仓库...");
+        System.out.println("用户C修改了代码，并提交到了中央仓库...");
     }
 }
